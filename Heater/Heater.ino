2e1ade2be198f802ff18630eb2c668d4c8e432b6
Chaim Solomon <chaim@itcentralstation.com>
2016-01-13 09:51:34 +0200

    Simplyfy and adjust to new relay
    The old relay board was too weak to handle frequent switching. Use
    heavy-duty relay for main PWM channel (9) - this one is high-active. And
    there is no need for changing the parameters in a menu - so removed the
    menu.

--- Heater/Heater.ino
+++ Heater/Heater.ino
@@ -15,15 +15,13 @@
 
 // Addresses in EEPROM for persistant storage
 #define EE_ON_OFF 0
-#define EE_KP 1
-#define EE_KI 2
 // Setpoint start address - this is a float, so it spans multiple bytes
 #define EE_SP 3
 
 // PID controller variables
 double Setpoint, Input, Output;
 // kP and kI parameters for controller
-int kp=0, ki=0;
+double kp=75, ki=0.5;
 
 // General device state - on or off
 int device_on = 0;
@@ -36,7 +34,7 @@ const int max_button = 100;
 int turnoff_counter = 0;
 
 // PID controller
-PID myPID(&Input, &Output, &Setpoint, 2, 0, 0, DIRECT);
+PID myPID(&Input, &Output, &Setpoint, 75, 0.5, 0, DIRECT);
 
 LiquidCrystal_I2C lcd(0x27,16,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -50,53 +48,55 @@ void callback()
 {
   if (device_on == 1) {  
     digitalWrite(11, LOW);
-    if (pwm_counter >= Output)
-    {
-      digitalWrite(9, HIGH);
-    }
-    else
-    {
-      digitalWrite(9, LOW);
-    }
     if (Output > 100) {
       if (pwm_counter > Output-100)
       {
-        digitalWrite(10, HIGH);
+        digitalWrite(10, LOW);//On
+        digitalWrite(9, LOW);//Off
       }
       else
       {
-        digitalWrite(10, LOW);
+        digitalWrite(10, LOW); //On
+        digitalWrite(9, HIGH);//On
       }
+    }
+  else if (pwm_counter >= Output )
+    {
+      digitalWrite(11, LOW);
+    digitalWrite(10, HIGH); //Off
+      digitalWrite(9, LOW); //Off
     }
     else
     {
-        digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH); //Off
+      digitalWrite(9, HIGH); // On
     }
   }
   else {
-      digitalWrite(9, HIGH);
-      digitalWrite(10, HIGH);
+      digitalWrite(9, LOW);//Off
+      digitalWrite(10, HIGH);//Off
       if (turnoff_counter > 0) {
-        digitalWrite(11, LOW);
+        digitalWrite(11, LOW);//On
       }
       else
       {
-        digitalWrite(11, HIGH);
+        digitalWrite(11, HIGH);//Off
       }
-      digitalWrite(12, HIGH);
+      digitalWrite(12, HIGH);//Off
   }
   pwm_counter += 1;
   if (pwm_counter > 100) pwm_counter = 0;
   if (turnoff_counter > 0) turnoff_counter--;
  
-  if (digitalRead(6) == 1)
-    {button_counter0 += 1;if (button_counter0 > max_button) button_counter0 = max_button; } 
+  if (digitalRead(6) == 1 )
+    {button_counter0 += 1; if (button_counter0 > max_button) button_counter0 = max_button; } 
     else { button_counter0 = 0;}
   if (digitalRead(7) == 1)
-    {button_counter1 += 1;if (button_counter1 > max_button) button_counter1 = max_button; } 
+    {button_counter1 += 1; if (button_counter1 > max_button) button_counter1 = max_button; } 
     else { button_counter1 = 0;}
   if (digitalRead(8) == 1)
-    {button_counter2 += 1;if (button_counter2 > max_button) button_counter2 = max_button; } 
+    {button_counter2 += 1; if (button_counter2 > max_button) button_counter2 = max_button; } 
     else { button_counter2 = 0;}
 
 }
@@ -111,44 +111,18 @@ void load_setpoint(void) {
   EEPROM.get(EE_SP, Setpoint);
 }
 
-int state = 0;
-void state_machine(void) {
-  switch (state) {
-  case 0: // Button 0: Next state, Button 1: On, Button 2: Off
-    if (button_counter0 > 2) {button_counter0 = 0; state = 1;}
-    if (button_counter1 > 2) {button_counter1 = 0; device_on = 1; EEPROM.update(EE_ON_OFF, device_on);} 
-    if (button_counter2 > 2) {button_counter2 = 0; device_on = 0; turnoff_counter = 3000; EEPROM.update(EE_ON_OFF, device_on);} 
-  case 1: // Button 0: Next state, Button 1: Increase temperature by 0.25C, Button 2: Decrease temperature by 0.25C
-    if (button_counter0 > 2) {button_counter0 = 0; state = 2;}
-    if (button_counter1 > 2) {button_counter1 = 0; Setpoint += 0.25;save_setpoint();} 
-    if (button_counter2 > 2) {button_counter2 = 0; Setpoint -= 0.25;save_setpoint();} 
-    break;
-  case 2: // Button 0: Next state, Button 1: Increase kP by 1, Button 2: Decrease kP by 1
-    if (button_counter0 > 2) {button_counter0 = 0; state = 3;}
-    if (button_counter1 > 2) {button_counter1 = 0; kp += 1; myPID.SetTunings(kp,ki,0); EEPROM.update(EE_KP, kp);} 
-    if (button_counter2 > 2) {button_counter2 = 0; kp -= 1; myPID.SetTunings(kp,ki,0); EEPROM.update(EE_KP, kp);} 
-    break;
-  case 3: // Button 0: Initial state, Button 1: Increase kI by 1, Button 2: Decrease kI by 1
-    if (button_counter0 > 2) {button_counter0 = 0; state = 0;}
-    if (button_counter1 > 2) {button_counter1 = 0; ki += 1; myPID.SetTunings(kp,ki,0); EEPROM.update(EE_KI, ki);} 
-    if (button_counter2 > 2) {button_counter2 = 0; ki -= 1; myPID.SetTunings(kp,ki,0); EEPROM.update(EE_KI, ki);} 
-    break;
-  default:
-    state = 0;
-    break;
-  }
-}
-
 void setup()
 {
-  state = 0;
   Timer1.initialize(100000);         // initialize timer1
   Timer1.attachInterrupt(callback);  // attaches callback() as a timer interrupt SR
   device_on =  EEPROM.read(EE_ON_OFF); // Initialize with values from EEPROM
-  kp = EEPROM.read(EE_KP);
-  ki = EEPROM.read(EE_KI);
   load_setpoint();
   int countdownMS = Watchdog.enable(20000); // Enable watchdog
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("HeaterControl");
+  lcd.setCursor(0,1);
+  lcd.print("Sensors");
 
   // Temperature sensor initialization
   sensors.begin();
@@ -158,9 +132,6 @@ void setup()
   lcd.init();                      // initialize the lcd 
   lcd.init();
   // Print a splash screen message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("HeaterControl");
   lcd.setCursor(0,1);
   lcd.print("Initializing");
   lcd.setCursor(0,0);
@@ -182,7 +153,7 @@ Input = sensors.getTempCByIndex(0);
  //turn the PID on
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(0, 200);
-  myPID.SetSampleTime(1000);
+  myPID.SetSampleTime(5000);
   myPID.SetTunings(kp,ki,0);
 }
 
@@ -191,8 +162,9 @@ void loop()
 {
 sensors.requestTemperatures(); // Send the command to get temperatures
 Input = sensors.getTempCByIndex(0);
-myPID.Compute();
-
+if (device_on == 1) {
+  myPID.Compute();
+}
 // First line: Input temperature, Set temperature and ON/OFF
 lcd.clear();
 lcd.setCursor(0,0);
@@ -212,12 +184,27 @@ else
 lcd.setCursor(0,1);
 lcd.print(Output);
 lcd.setCursor(7,1);
-lcd.print(kp);
-lcd.setCursor(12,1);
-lcd.print(ki);
-lcd.setCursor(15,1);
-lcd.print(state);
-state_machine();
+//lcd.print(kp);
+//lcd.setCursor(12,1);
+lcd.print(digitalRead(6));
+
+// Buttons
+if (button_counter0 > 2) {
+  button_counter0 = 0; 
+  if (device_on == 0)
+  {
+    device_on = 1;
+  }
+  else
+  {
+    device_on = 0;
+    Output = 0;
+    turnoff_counter = 10000;
+  };
+  EEPROM.update(EE_ON_OFF, device_on);
+  } 
+if (button_counter1 > 2) {button_counter1 = 0; Setpoint += 0.25; save_setpoint();} 
+if (button_counter2 > 2) {button_counter2 = 0; Setpoint -= 0.25; save_setpoint();} 
 
 Watchdog.reset();
 }
